8f31d5841cb184e92606aef5f0b533ce600a72e4
Estefanía <estefaniasalamanca@hotmail.com>
2023-03-30 17:15:23 +0200

    A new button is created for a new app

--- app/src/main/java/com/estefaniasalamanca/formacion/MenuActivity.kt
+++ app/src/main/java/com/estefaniasalamanca/formacion/MenuActivity.kt
@@ -1,11 +1,12 @@
 package com.estefaniasalamanca.formacion
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.estefaniasalamanca.formacion.FirstApp.FirstAppActivity
-import android.content.Intent
 import com.estefaniasalamanca.formacion.IMCApp.ImcCalculatorActivity
+import com.estefaniasalamanca.formacion.ToDoApp.ToDoActivity
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,8 +17,18 @@ class MenuActivity : AppCompatActivity() {
         btnSaludApp.setOnClickListener { navigateToSaludApp() }
         val btnIMCApp = findViewById<Button>(R.id.btnIMCApp)
         btnIMCApp.setOnClickListener { navigateToIMCApp() }
+        val btnToDoApp = findViewById<Button>(R.id.btnToDoApp)
+        btnToDoApp.setOnClickListener { navigateToToDoApp() }
+
     }
-     private fun navigateToSaludApp(){
+
+    private fun navigateToToDoApp() {
+        val intent = Intent(this, ToDoActivity::class.java)
+        startActivity(intent)
+
+    }
+
+    private fun navigateToSaludApp(){
          val intent = Intent(this, FirstAppActivity::class.java)
          startActivity(intent)
      }
---
+++ app/src/main/java/com/estefaniasalamanca/formacion/ToDoApp/ToDoActivity.kt
@@ -0,0 +1,12 @@
+package com.estefaniasalamanca.formacion.ToDoApp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.estefaniasalamanca.formacion.R
+
+class ToDoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_to_do)
+    }
+}
