44a367ad8a4865f33b1035e4b94a2b5da3d689c6
Estefanía <estefaniasalamanca@hotmail.com>
2023-04-05 12:08:20 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/values/colors.xml

---
+++ app/src/main/java/com/estefaniasalamanca/formacion/settings/SettingsModel.kt
@@ -0,0 +1,9 @@
+package com.estefaniasalamanca.formacion.settings
+
+data class SettingsModel(
+    var volume: Int,
+    var bluetooth: Boolean,
+    var darkMode: Boolean,
+    var vibration: Boolean
+)
+
